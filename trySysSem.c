4af38dcce9965cd75ec321ef86f6dc4d09adb999
xAKLx <xaklx20@gmail.com>
2016-11-13 18:24:03 -0400

    Add test for see

---
+++ trySysSem.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <minix/syslib.h>
+#include <lib.h>    // provides _syscall and message
+ 
+int main(int argc, char **argv)
+{
+	if(argc < 1)
+		return 1;
+ 
+ 	message m;
+
+ 	m.m1_i1 = atoi(argv[0]);
+
+ 	int i = _syscall(PM_PROC_NR, PM_SEM_CREATE, &m);
+
+ 	printf("Syscall create equals %d", i);
+
+ 	i = _syscall(PM_PROC_NR, PM_SEM_DOWN, &m);
+
+ 	printf("Syscall down equals %d", i);
+
+ 	i = _syscall(PM_PROC_NR, PM_SEM_CREATE, &m);
+
+ 	printf("Syscall createequals %d", i);
+
+    return 0;
+}
