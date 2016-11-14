6a8e8ff4258efe6cbf6c365e1b7ede62e3c45bbe
xAKLx <xaklx20@gmail.com>
2016-11-13 22:02:49 -0400

    modify test

---
+++ trySysSem2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <minix/syslib.h>
+#include <lib.h>    // provides _syscall and message
+ 
+int main(int argc, char **argv)
+{
+	if(argc < 1)
+		return 1;
+ 
+ 	message m;
+
+ 	m.m1_i1 = atoi(argv[1]);
+
+  printf("\nPid from test = %d", getpid());
+
+ 	printf("\nArgument to send %d", atoi(argv[1]));
+
+ 	int i = _syscall(PM_PROC_NR, PM_SEM_CREATE, &m);
+
+    return 0;
+}
